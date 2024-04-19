bceccdc28e3fa3202a2f3ad1b33165c729261ac5
Karl Kevin C Bacon <karlkevincbacon@gmail.com>
2024-04-19 11:05:59 +0800

    Initial Commit

---
+++ cp2/mydb.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = '12345'
+)
+
+cursorObject = dataBase.cursor()
+cursorObject.execute("CREATE DATABASE crud")
+
+print("ALL DONE!")
